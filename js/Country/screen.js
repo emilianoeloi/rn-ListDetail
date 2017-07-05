214f227842ffe24498f9e60aae173eede597d501
Emiliano Barbosa <emilianoeloi@gmail.com>
2017-07-04 21:52:52 -0300

    CHANGE: add interest point in country screen

--- js/Country/screen.js
+++ js/Country/screen.js
@@ -8,6 +8,7 @@ import {
   FlatList,
   ScrollView,
   TouchableHighlight,
+  Linking,
 } from 'react-native';
 import {
   Card,
@@ -21,6 +22,7 @@ import {
   Title,
   Right,
   Icon,
+  Thumbnail,
 } from 'native-base';
 
 import {
@@ -31,10 +33,11 @@ import styles from './styles';
 
 class CountryScreen extends Component {
   render() {
-    const {goBack} = this.props.navigation;
+    const { goBack } = this.props.navigation;
     const { params } = this.props.navigation.state;
     const { country } = params;
     const photoURI = `https://bodega-a35c0.firebaseapp.com/${country.key}/points/${country.pointsOfInterest[0].photo}`;
+    const flagURI = `https://bodega-a35c0.firebaseapp.com/${country.key}/flag/${country.flag}`;
     const theme = {
       backgroundColor: country.colors[0],
     };
@@ -51,6 +54,7 @@ class CountryScreen extends Component {
       </View>
     )
     const json = JSON.stringify(country);
+    console.info('this.flagURI', flagURI);
     return (
       <Container>
         <Header>
@@ -77,6 +81,29 @@ class CountryScreen extends Component {
                   {country.geography}
                 </Text>
               </Body>
+            </CardItem>
+          </Card>
+          <Card>
+            <CardItem>
+              <Left>
+                <Thumbnail source={{uri:flagURI}} />
+                <Body>
+                  <Text>{country.pointsOfInterest[0].name}</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Image source={{uri: photoURI}} style={{resizeMode: 'contain', height: 200, width: null, flex: 1}}/>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent onPress={() => {
+                  Linking.openURL(country.pointsOfInterest[0].moreUrl)
+                }}>
+                  <Icon active name="navigate" />
+                  <Text>Saiba Mais</Text>
+                </Button>
+              </Left>
             </CardItem>
           </Card>
         </Content>
